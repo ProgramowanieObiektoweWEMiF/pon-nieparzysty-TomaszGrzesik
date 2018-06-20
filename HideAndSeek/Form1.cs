cebe017c5b15162e3194a99f7563c47485e3eb93
TomaszGrzesik <t.grzesik19@gmail.com>
2018-06-20 14:14:11 +0200

    Add files via upload

---
+++ HideAndSeek/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HideAndSeek
+{
+    public partial class Form1 : Form
+    {
+        int Moves;
+
+        Location currentLocation;
+
+        RoomWithDoor livingRoom;
+        RoomWithHidingPlace diningRoom;
+        RoomWithDoor kitchen;
+        Room stairs;
+        RoomWithHidingPlace hallway;
+        RoomWithHidingPlace bathroom;
+        RoomWithHidingPlace masterBedroom;
+        RoomWithHidingPlace secondBedroom;
+
+        OutsideWithDoor frontYard;
+        OutsideWithDoor backYard;
+        OutsideWithHidingPlace garden;
+        OutsideWithHidingPlace driveway;
+
+        Opponent opponent;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CreateObjects();
+            opponent = new Opponent(frontYard);
+            ResetGame(false);
+        }
+
+        private void MoveToANewLocation(Location newLocation)
+        {
+            Moves++;
+            currentLocation = newLocation;
+            RedrawForm();
+        }
+
+        private void RedrawForm()
+        {
+            exits.Items.Clear();
+            for (int i = 0; i < currentLocation.Exits.Length; i++)
+                exits.Items.Add(currentLocation.Exits[i].Name);
+            exits.SelectedIndex = 0;
+            description.Text = currentLocation.Description + "\r\n(Ruchy #" + Moves + ")";
+            if (currentLocation is IHidingPlace)
+            {
+                IHidingPlace hidingPlace = currentLocation as IHidingPlace;
+                check.Text = "Sprawdź " + hidingPlace.HidingPlaceName;
+                check.Visible = true;
+            }
+            else
+                check.Visible = false;
+            if (currentLocation is IHasExteriorDoor)
+                goThroughTheDoor.Visible = true;
+            else
+                goThroughTheDoor.Visible = false;
+        }
+
+        private void CreateObjects()
+        {
+            livingRoom = new RoomWithDoor("Salon", "andtyczny dywan",
+                      "w komodzie", "dębowe drzwi");
+            diningRoom = new RoomWithHidingPlace("Jadalnia", "złoty świecznik",
+                       " pod stołem ");
+            kitchen = new RoomWithDoor("Kuchnia", "srebrną zastawę",
+                      "pod zlewem", "szklane drzwi");
+            stairs = new Room("Schody", "drewniana szafka");
+            hallway = new RoomWithHidingPlace("Poddasze", "zdjęcie kota",
+                      "w szafce");
+            bathroom = new RoomWithHidingPlace("Łazienka", "złote krany",
+                      "pod prysznicem");
+            masterBedroom = new RoomWithHidingPlace("Główna sypialnia", "ogromne łóżko",
+                      "pod łóżkiem");
+            secondBedroom = new RoomWithHidingPlace("Druga sypialnia", "małe łóżko",
+                      "pod łóżkiem");
+
+            frontYard = new OutsideWithDoor("Pole przed domem", false, "Dębowe drzwi");
+            backYard = new OutsideWithDoor("Pole za domem", true, "szklane drzwi");
+            garden = new OutsideWithHidingPlace("Pole (ogródek)", false, "w krzakach");
+            driveway = new OutsideWithHidingPlace("Podjazd", true, "w garażu");
+
+            diningRoom.Exits = new Location[] { livingRoom, kitchen };
+            livingRoom.Exits = new Location[] { diningRoom, stairs };
+            kitchen.Exits = new Location[] { diningRoom };
+            stairs.Exits = new Location[] { livingRoom, hallway };
+            hallway.Exits = new Location[] { stairs, bathroom, masterBedroom, secondBedroom };
+            bathroom.Exits = new Location[] { hallway };
+            masterBedroom.Exits = new Location[] { hallway };
+            secondBedroom.Exits = new Location[] { hallway };
+            frontYard.Exits = new Location[] { backYard, garden, driveway };
+            backYard.Exits = new Location[] { frontYard, garden, driveway };
+            garden.Exits = new Location[] { backYard, frontYard };
+            driveway.Exits = new Location[] { backYard, frontYard };
+
+            livingRoom.DoorLocation = frontYard;
+            frontYard.DoorLocation = livingRoom;
+
+            kitchen.DoorLocation = backYard;
+            backYard.DoorLocation = kitchen;
+        }
+
+        private void ResetGame(bool displayMessage)
+        {
+            if (displayMessage)
+            {
+                MessageBox.Show("znalazłeś mnie w" + Moves + " ruchach");
+                IHidingPlace foundLocation = currentLocation as IHidingPlace;
+                description.Text = "Znalazłeś przeciwnika w  " + Moves
+                      + " ruchach! Był w " + foundLocation.HidingPlaceName + ".";
+            }
+            Moves = 0;
+            hide.Visible = true;
+            goHere.Visible = false;
+            check.Visible = false;
+            goThroughTheDoor.Visible = false;
+            exits.Visible = false;
+        }
+
+        private void check_Click(object sender, EventArgs e)
+        {
+            Moves++;
+            if (opponent.Check(currentLocation))
+                ResetGame(true);
+            else
+                RedrawForm();
+        }
+
+        private void hide_Click(object sender, EventArgs e)
+        {
+            hide.Visible = false;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                opponent.Move();
+                description.Text = i + "... ";
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(200);
+            }
+
+            description.Text = "Gotowy czy nie, szukam!";
+            Application.DoEvents();
+            System.Threading.Thread.Sleep(500);
+
+            goHere.Visible = true;
+            exits.Visible = true;
+            MoveToANewLocation(livingRoom);
+        }
+
+        private void goHere_Click(object sender, EventArgs e)
+        {
+            MoveToANewLocation(currentLocation.Exits[exits.SelectedIndex]);
+        }
+
+        private void goThroughTheDoor_Click(object sender, EventArgs e)
+        {
+            IHasExteriorDoor hasDoor = currentLocation as IHasExteriorDoor;
+            MoveToANewLocation(hasDoor.DoorLocation);
+        }
+
+
+    }
+}
